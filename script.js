ac1d25adccc473ab3e759dde7f964330c9de28c4
Swapnil kota <83860595+Swapnilkota108@users.noreply.github.com>
2021-08-15 18:36:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,23 +4,38 @@ const purchasePrice = document.querySelector('#purchase-input');
 const button = document.querySelector("#button");
 const output = document.querySelector('#output')
 
+output.style.display = 'none';
+
+
 button.addEventListener('click',function (){
-    validQuantity();
+    output.style.display = 'block'
     let prc = purchasePrice.value;
- let qty = quantity.value;
- let slg = sellingPrice.value;
-(calculateProfitOrLoss(prc,qty,slg));
+    let qty = quantity.value;
+    let slg = sellingPrice.value;
+    // validQuantity(prc,qty,slg);
+    calculateProfitOrLoss(prc,qty,slg);
+
+
 })
 
 
-function validQuantity(){
-    if(quantity.value<=0){
-     output.innerText = 'The Quantity of Stock should be atleast greater than 0'
-    }
-}
+// function validQuantity(prc,qty,slg){
+    
+
+//     if(qty<=0){
+//         output.innerText = 'The Quantity of the Stock should be atleast greater than 0';
+//        }
+//     if(typeof(prc&&slg) != 'number'){
+//         output.innerText = 'Invalid input'
+//     } 
+    
+
+ 
+    
+// }
 
 function calculateProfitOrLoss(prc,qty,slg){
- 
+    
  let totalPurchaseAmount = prc*qty;
  let totalSellingAmount = slg*qty;
  
@@ -28,10 +43,10 @@ function calculateProfitOrLoss(prc,qty,slg){
      let profittedAmount = totalSellingAmount-totalPurchaseAmount;
      let percentage = ((slg-prc)*100)/prc;
      output.innerText = `You gained ${percentage}%. Your total profit is ₹${profittedAmount}  `
- }  if(prc<slg){
+ }  if(prc>slg){
     let lossAmount = totalPurchaseAmount-totalSellingAmount;
     let percentage = ((prc-slg)*100/prc);
     output.innerText = `You lost ${percentage}%. Your total loss is ₹${lossAmount} `
-
  }
+ 
 }
